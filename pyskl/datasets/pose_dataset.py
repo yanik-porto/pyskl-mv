40ce50f7bf6a36982ed51deadd7db78adf35db54
yanik-porto <yanik.porto@gmail.com>
2024-03-14 15:21:48 +0100

    merge views after preprocessing

--- pyskl/datasets/pose_dataset.py
+++ pyskl/datasets/pose_dataset.py
@@ -2,6 +2,7 @@
 import mmcv
 import os.path as osp
 import numpy as np
+import torch
 
 from ..utils import get_root_logger
 from .base import BaseDataset, get_group
@@ -121,9 +122,11 @@ class PoseDatasetMV(PoseDataset):
                  mc_cfg=('localhost', 22077),
                  pair_mode=False,
                  is_split_by_group=True,
+                 transform_before_merge=True,
                  **kwargs):
         self.pair_mode = pair_mode
         self.is_split_by_group = is_split_by_group
+        self.transform_before_merge = transform_before_merge
         super().__init__(
             ann_file, pipeline, split, valid_ratio, box_thr, class_prob, memcached, mc_cfg, **kwargs)
 
@@ -146,7 +149,7 @@ class PoseDatasetMV(PoseDataset):
         
         return concat
 
-    def data_from_split(self, data, split):
+    def datamv_from_split(self, data, split):
         datamv = {}
 
         for annot in data:
@@ -158,43 +161,36 @@ class PoseDatasetMV(PoseDataset):
             
             if group not in datamv:
                 # copie first annotation in the group
-                datamv[group] = self.create_new_group_annot(annot, group)
-                continue
-
-            annotmv = datamv[group]
-
-
-            annotmv = self.merge_annot_into_other(annot, annotmv)
-
-        # remove sequence with wrong number of views or persons
-        keytorem = []
-        for key, amv in datamv.items():
-            if amv['keypoint'].shape[0] != 3 and amv['keypoint'].shape[0] != 6:
-                print('watchout: num of view is different from 3 : ', amv['keypoint'].shape[0])
-                keytorem.append(key)
-        for key in keytorem:
-            del datamv[key]
-
-        datamv = list(datamv.values())
-        return datamv
-    
-    def data_pair_from_split(self, data, split):
-        datamv = {}
-
-        for annot in data:
-            group = get_group(annot['frame_dir'])
-
-            # do not take data not in this split
-            if group not in split:
-                continue
-
-            if group not in datamv:
                 datamv[group] = []
 
             if annot['keypoint'].shape[0] != 1 and annot['keypoint'].shape[0] != 2:
                 print("different : ", annot['keypoint'].shape)
 
             datamv[group].append(annot)
+
+        return datamv
+
+    def data_from_split(self, data, split):
+        datamv = self.datamv_from_split(data, split)
+        
+        idxToRem = []
+        for idx, datas in enumerate(datamv):
+            if len(datas) != 3:
+                print('watchout: num of view is different from 3 : ', len(datamv))
+                idxToRem.append(idx)
+        
+        for idx in sorted(idxToRem, reverse=True):
+            del datamv[idx]
+
+        return datamv
+
+    def data_pair_from_split(self, data, split):
+
+
+
+
+
+        datamv = self.datamv_from_split(data, split)
 
         idxPairs = {0: 1, 1: 2, 2: 0}
         pairs = []
@@ -206,17 +202,45 @@ class PoseDatasetMV(PoseDataset):
             for i in range(3):
                 annot = datas[i]
                 annot_linked = datas[idxPairs[i]]
-                annot = self.merge_annot_into_other(annot_linked, annot)
-                pairs.append(annot)
+                pairs.append((annot, annot_linked))
 
         idxToRem = []
         for idx, pair in enumerate(pairs):
-            if pair['keypoint'].shape[0] != 2 and pair['keypoint'].shape[0] != 4:
-                print('watchout: num of view is different from 2 : ', pair['keypoint'].shape[0])
+            if len(pair) != 2:
+                print('watchout: num of view is different from 2 : ', len(pair))
                 idxToRem.append(idx)
         
         for idx in sorted(idxToRem, reverse=True):
             del pairs[idx]
+
+        return pairs
+    
+    def merge_datamv(self, datamv):
+        annotsmv = []
+        for group, datas in datamv.items():
+            annotmv = self.create_new_group_annot(datas[0], group)
+            for idx in range(1, len(datas)):
+                annot = datas[idx]
+                annotmv = self.merge_annot_into_other(annot, annotmv)
+                annotsmv.append(annotmv)
+
+        for amv in annotsmv:
+            if amv['keypoint'].shape[0] != 3 and amv['keypoint'].shape[0] != 6:
+                print('watchout: num of view is different from 3 : ', amv['keypoint'].shape[0])
+
+        return annotsmv
+
+    def merge_pairs(self, pairs):
+        pairs_merged = []
+        for pair in pairs:
+            annot = pair[0]
+            annot_linked = pair[1]
+            annot = self.merge_annot_into_other(annot_linked, annot)
+            pairs_merged.append(annot)
+
+        for pair in pairs:
+            isdatacorrect = pair['keypoint'].shape[0] != 2 and pair['keypoint'].shape[0] != 4
+            assert isdatacorrect, "wrong keypoint shape : " + str(pair['keypoint'].shape[0])
 
         return pairs
 
@@ -232,6 +256,9 @@ class PoseDatasetMV(PoseDataset):
 
             data = self.data_pair_from_split(data, split) if self.pair_mode else self.data_from_split(data, split)
 
+            if not self.transform_before_merge:
+                data = self.merge_pairs(data) if self.pair_mode else self.merge_datamv(data)
+
         for item in data:
             # Sometimes we may need to load anno from the file
             if 'filename' in item:
@@ -239,3 +266,21 @@ class PoseDatasetMV(PoseDataset):
             if 'frame_dir' in item:
                 item['frame_dir'] = osp.join(self.data_prefix, item['frame_dir'])
         return data
+    
+    def merge_results_trans(self, results_trans):
+        return torch.cat(results_trans)
+    
+    def prepare_train_frames(self, idx):
+        results = copy.deepcopy(self.video_infos[idx])
+
+        if self.transform_before_merge:
+            results_trans = []
+            for res in results:
+                res_trans = self.prepare_train_frames_for_results(res)
+                print(res_trans.shape)
+                results_trans.append(res_trans)
+            return self.merge_results_trans(results_trans)
+
+        else:
+            return self.prepare_train_frames_for_results(results)
+        
